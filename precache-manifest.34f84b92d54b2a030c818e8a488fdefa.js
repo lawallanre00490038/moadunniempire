2521085fb047a7cd572ee6d837e826db82ea9502
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2023-05-01 12:27:36 +0100

    Updates

---
+++ precache-manifest.34f84b92d54b2a030c818e8a488fdefa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4cafdb14efea87707ca",
+    "url": "/moadunniempire/static/css/main.66e56e7d.chunk.css"
+  },
+  {
+    "revision": "d4cafdb14efea87707ca",
+    "url": "/moadunniempire/static/js/main.d4cafdb1.chunk.js"
+  },
+  {
+    "revision": "bc8d59f062184611f0d3",
+    "url": "/moadunniempire/static/js/1.bc8d59f0.chunk.js"
+  },
+  {
+    "revision": "3f17e625c473c51da8d4",
+    "url": "/moadunniempire/static/js/runtime~main.3f17e625.js"
+  },
+  {
+    "revision": "f8015686488109640aea6f6fb766c147",
+    "url": "/moadunniempire/index.html"
+  }
+];
